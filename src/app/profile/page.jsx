2022fc5db5cdbc344e34613205557da69f45054e
Saranya Kumar <kumarsaranya818@gmail.com>
2023-06-18 20:41:00 +0530

    branch created

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,7 +1,7 @@
 import "./profile.scss";
 
 const Profile = () => {
-  return <section className="profile-container">profile</section>;
+  return <section className="profile-container">Profile Name</section>;
 };
 
 export default Profile;
