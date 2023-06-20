cb9ca49adc8eb2b90ec444cc8cf96433a1eea6da
Sayan <sayanmunshi36@gmail.com>
2023-06-20 12:50:28 +0530

    User Login and Signup Functions

--- src/app/components/register/Register.jsx
+++ src/app/components/register/Register.jsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import "./register.scss";
 import Link from "next/link";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -10,67 +13,106 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    try {
+      if (password !== confirmPassword) {
+        return toast.error("Password and Confirm Password not matched");
+      }
+      setLoading(true);
+      const res = await axios.post(
+        "https://ecommerce-backend-neo8.onrender.com/api/v1/users/register",
+        {
+          name,
+          email,
+          password,
+          username,
+        }
+      );
+      localStorage.setItem("userInfo", JSON.stringify(res.data));
+      setName("");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+      setUsername("");
+      setLoading(false);
+      toast.success("User Registered Successfully");
+      router.replace("/");
+    } catch (error) {
+      const { message } = JSON.parse(error.request.response);
+      setLoading(false);
+      toast.error(message);
+    }
   };
 
+  if (localStorage.userInfo) router.replace("/");
+
+  if (loading) return <p>Loading...</p>;
+
   return (
-    <section className="signup--page">
-      <h2 className="signup--page__header font-bold">Sign Up</h2>
-      <form className="signup--page__form" onSubmit={handleSubmit}>
-        <input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="signup--page__form__input"
-          type="text"
-          placeholder="Enter Your Name"
-          required
-        />
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="signup--page__form__input"
-          type="email"
-          placeholder="Enter Your Email"
-          required
-        />
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="signup--page__form__input"
-          type="password"
-          placeholder="Enter Password"
-          required
-        />
-        <input
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          className="signup--page__form__input"
-          type="password"
-          placeholder="Confirm Password"
-          required
-        />
-        <input
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="signup--page__form__input"
-          type="text"
-          placeholder="Enter an unique Username"
-          required
-        />
-        <button type="submit" className="signup--page__form__btn mt-2 mb-4">
-          Sign Up
-        </button>
-      </form>
-      <p className="mt-4 text-[#c1c1c1]">Already have an Account ?</p>
-      <Link href="/login" className="mt-2 underline">
-        Log In
-      </Link>
-      <footer className="footer mt-8 text-xs text-[#c1c1c1]">
-        Ecommerce Name © {new Date().getFullYear()}
-      </footer>
-    </section>
+    <>
+      <section className="signup--page">
+        <h2 className="signup--page__header font-bold">Sign Up</h2>
+        <form className="signup--page__form" onSubmit={handleSubmit}>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="signup--page__form__input"
+            type="text"
+            placeholder="Enter Your Name"
+            required
+          />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="signup--page__form__input"
+            type="email"
+            placeholder="Enter Your Email"
+            required
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="signup--page__form__input"
+            type="password"
+            placeholder="Enter Password"
+            required
+          />
+          <input
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="signup--page__form__input"
+            type="password"
+            placeholder="Confirm Password"
+            required
+          />
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="signup--page__form__input"
+            type="text"
+            placeholder="Enter an unique Username"
+            required
+          />
+          <button type="submit" className="signup--page__form__btn mt-2 mb-4">
+            Sign Up
+          </button>
+        </form>
+        <p className="mt-4 text-[#c1c1c1]">Already have an Account ?</p>
+        <Link href="/login" className="mt-2 underline">
+          Log In
+        </Link>
+        <footer className="footer mt-8 text-xs text-[#c1c1c1]">
+          Ecommerce Name © {new Date().getFullYear()}
+        </footer>
+      </section>
+      <ToastContainer />
+    </>
   );
 };
 
