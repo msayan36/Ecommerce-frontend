9eb0e108d941a7237568d4f3c4e1bd72a30587f8
Sayan <sayanmunshi36@gmail.com>
2023-06-22 22:36:11 +0530

    Code Correction

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,19 +1,6 @@
-// import Image from "next/image";
-// import Link from "next/link";
-// import "./profile.scss";
-// import "../global.css";
-// import "./page.css";
-// import WhiteBtn from "../components/WhiteBtn";
-// import Navbar from "../components/navbar/Navbar";
-// import ProfileDetails from "../components/ProfileDetails";
-// import { IoIosSettings } from "react-icons/io";
-// import { AiFillStar } from "react-icons/ai";
-// import ProfileImg from "./images/profile.png";
-// import ProdImg from "./images/prod_img.png";
 import Profile from "../components/profile/Profile";
 
 const page = () => {
-  let img = [1, 2, 3, 4, 5];
   return <Profile />;
 };
 export default page;
